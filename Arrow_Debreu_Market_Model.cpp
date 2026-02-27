3ddc805186f57e423d4830d0348606d9d57da12a
RobXu <ooorobertx@gmail.com>
2026-02-27 02:13:29 -0500

    test push

--- Arrow_Debreu_Market_Model.cpp
+++ Arrow_Debreu_Market_Model.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <vector>
+using namespace std;
 
 int main() {
     std::cout << "Arrow Debreu One Period Market Model" << std::endl;
+    vector<vector<double>> U = {
+        {2, 1, 1},
+        {0, 3, 2},
+        {0, 0, 4}
+    };
     return 0;
 }
